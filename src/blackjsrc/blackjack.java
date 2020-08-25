cc86f40b13f61fc8e2e9ef46662fe8135582d981
long-stephen-michael <long.stephen.michael@gmail.com>
2020-08-25 00:36:25 -0400

    added Two

--- src/blackjsrc/blackjack.java
+++ src/blackjsrc/blackjack.java
@@ -6,7 +6,7 @@ public class blackjack {
 		int[] deck = new int[52];
 		
 		String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
-	    String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9",
+	    String[] ranks = {"Ace", "Two", "3", "4", "5", "6", "7", "8", "9",
 	      "10", "Jack", "Queen", "King"};
 	        
 	    // Initialize cards
